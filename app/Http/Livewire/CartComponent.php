462e5c5639c541bca3bf272f994ac8b18b34d324
Roshan Lasiwa <roshanlasiwa@gmail.com>
2021-05-25 13:37:52 +0545

    added Delete Card and Destroy all

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,31 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
 
 class CartComponent extends Component
 {
+    public function increaseQuantity($rowId){
+        $product = Cart::get($rowId);
+        $qty = $product->qty + 1;
+        Cart::update($rowId,$qty);
+    }
+
+    public function decreaseQuantity($rowId){
+        $product = Cart::get($rowId);
+        $qty = $product-> qty -1;
+        Cart::update($rowId,$qty);
+    }
+    
+    public function deleteCart($rowId){
+        Cart::remove($rowId);
+        session()->flash('success_message','Item has been Removed');
+    }
+
+    public function deleteAllCart(){
+        Cart::destroy();
+    }
+
     public function render()
     {
         return view('livewire.cart-component')->layout('layouts.base');

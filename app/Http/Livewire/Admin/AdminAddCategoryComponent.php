2272952b7d0d91dead6ccae205cb7181fd9efe51
Roshan Lasiwa <roshanlasiwa@gmail.com>
2021-05-27 22:39:39 +0545

    added new component view and component

---
+++ app/Http/Livewire/Admin/AdminAddCategoryComponent.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Illuminate\Support\Str;
+use Livewire\Component;
+use App\Models\Category;
+
+
+class AdminAddCategoryComponent extends Component
+{
+    public $name;
+    public $slug;
+
+    public function generateslug(){
+        $this->slug = Str::slug($this->name);
+    }
+
+    public function storeCategory(){
+        $category = new Category();
+        $category->name = $this->name;
+        $category->slug = $this->slug;
+        $category->save();
+        session()->flash('message','Category has been created Successfully');
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.admin-add-category-component')->layout('layouts.base');
+    }
+}

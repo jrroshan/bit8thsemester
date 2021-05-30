4fbbcbc99c027162205fea0d4b271cbbcf6f2f12
Roshan Lasiwa <roshanlasiwa@gmail.com>
2021-05-30 12:48:16 +0545

    Added Add Products Route

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -80,6 +80,10 @@
 
 													</li>
 													<li class="menu-item">
+														<a title="Categories" href="{{route('admin.products')}}">Products</a>
+
+													</li>
+													<li class="menu-item">
 														<a href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
 
 													</li>

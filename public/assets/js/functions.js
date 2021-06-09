70886261b549d7debf3f023fe4484741c7c3d193
Roshan Lasiwa <roshanlasiwa@gmail.com>
2021-06-09 18:05:19 +0545

    from this.choosen to this.select2

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -77,7 +77,7 @@
 			}
 			if($("select:not(.except-chosen)").length > 0){
 				$("select:not(.except-chosen)").each(function(){
-					$(this).chosen();
+					$(this).select2();
 				});
 			}
         },

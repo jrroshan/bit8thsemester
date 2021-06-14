3b461704b57523cc7a96a35b0eb495823bc4509c
Roshan Lasiwa <roshanlasiwa@gmail.com>
2021-06-14 15:18:43 +0545

    make dynamic add to cart

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -8,24 +8,29 @@ use Cart;
 class CartComponent extends Component
 {
     public function increaseQuantity($rowId){
-        $product = Cart::get($rowId);
+        $product = Cart::instance('cart')->get($rowId);
         $qty = $product->qty + 1;
-        Cart::update($rowId,$qty);
+        Cart::instance('cart')->update($rowId,$qty);
+        $this->emitTo('cart-count-component','refreshComponent');
     }
 
     public function decreaseQuantity($rowId){
-        $product = Cart::get($rowId);
+        $product = Cart::instance('cart')->get($rowId);
         $qty = $product-> qty -1;
-        Cart::update($rowId,$qty);
+        Cart::instance('cart')->update($rowId,$qty);
+        $this->emitTo('cart-count-component','refreshComponent');
     }
     
     public function deleteCart($rowId){
-        Cart::remove($rowId);
+        Cart::instance('cart')->remove($rowId);
+        $this->emitTo('cart-count-component','refreshComponent');
         session()->flash('success_message','Item has been Removed');
+
     }
 
     public function deleteAllCart(){
-        Cart::destroy();
+        Cart::instance('cart')->destroy();
+        $this->emitTo('cart-count-component','refreshComponent');
     }
 
     public function render()

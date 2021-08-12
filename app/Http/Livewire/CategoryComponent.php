41336d343064df2cd2592ef83fc351a9ab04a9f0
Roshan Lasiwa <roshanlasiwa@gmail.com>
2021-08-12 21:37:09 +0545

    add instance cart

--- app/Http/Livewire/CategoryComponent.php
+++ app/Http/Livewire/CategoryComponent.php
@@ -22,7 +22,7 @@ class CategoryComponent extends Component
     }
 
     public function store($product_id,$product_name,$product_price){
-        Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        Cart::instance('cart')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
         session()->flash('success_message','item added in Cart');
         return redirect()->route('product.cart');
     }
